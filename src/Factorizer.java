d04756c20ded2ef815aeb62f4ce136b6701eed7b
ajlanza <61472953+ajlanza@users.noreply.github.com>
2020-12-21 16:27:41 -0500

    class StringFormatter and method truncate added

--- src/Factorizer.java
+++ src/Factorizer.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Factorizer {
+public class Factorizer {
+    public static void main(String[] args) {
+        int numberA = 3;
+        int numberB = 9;
+
+        Factorizer factorizer = new Factorizer();
+
+        boolean result = factorizer.isFactor(numberA, numberB);
+
+        if (result) {
+            System.out.printf("%d is a factor of %d\n", numberA, numberB);
+        } else {
+            System.out.printf("%d is not a factor of %d\n", numberA, numberB);
+        }
+    }
+
+    public boolean isFactor(int a, int b){
+        int ans = b % a;
+        return ans == 0;
+    }
 }
